a2a22021114fde656ab0692eb4c8c77bc6695288
Dedal378 <orlovetsfoto@gmail.com>
2021-10-08 14:04:28 +0300

    add comp

---
+++ src/components/request/RequestTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+    </tr>
+    </thead>
+  </table>
+</template>
+<script>
+export default {
+  name: 'RequestTable',
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список заявок">
+    <template #header>
+      <button class="btn primary">
+        Создать
+      </button>
+    </template>
+
+    <RequestTable />
+  </app-page>
 </template>
 
 <script>
+import AppPage from '@/components/ui/AppPage.vue'
+import RequestTable from "@/components/request/RequestTable.vue";
+
 export default {
   name: 'Home',
+  components: { RequestTable, AppPage }
 }
 </script>
