d2e76b6f7bf87ba9b284b90044c1a9557dddcfa3
Dedal378 <orlovetsfoto@gmail.com>
2021-10-08 14:25:35 +0300

    add modal comp

---
+++ src/components/ui/AppModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')"></div>
+  <div class="modal">
+    <h3 v-if="title">{{ title }}</h3>
+
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal",
+  props: {
+    title: {
+      type: String
+    }
+  },
+  emits: ['close']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,37 @@
 <template>
   <app-page title="Список заявок">
     <template #header>
-      <button class="btn primary">
+      <button @click="modal = true" class="btn primary">
         Создать
       </button>
     </template>
 
     <request-table :requests="[]"></request-table>
+
+    <teleport to="body">
+      <app-modal v-if="modal" @close="modal = false" title="Создать заявку">
+        <request-modal />
+      </app-modal>
+    </teleport>
   </app-page>
 </template>
 
 <script>
 import AppPage from '@/components/ui/AppPage.vue'
 import RequestTable from "@/components/request/RequestTable.vue";
+import AppModal from "@/components/ui/AppModal.vue";
+import RequestModal from "@/components/request/RequestModal.vue";
+import { ref } from "vue";
 
 export default {
   name: 'Home',
-  components: { RequestTable, AppPage }
+  components: { RequestTable, AppPage, AppModal, RequestModal },
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal
+    }
+  },
 }
 </script>
