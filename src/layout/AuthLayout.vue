c18fdf3366ac72a9f4bab44293a07de64aa66798
Dedal378 <orlovetsfoto@gmail.com>
2021-10-08 13:27:07 +0300

    вывод ошибок

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container">
+    <AppMessage />
     <router-view />
   </div>
 </template>
 
 <script>
+import AppMessage from '@/components/ui/AppMessage.vue'
+
 export default {
-  name: "AuthLayout"
+  name: "AuthLayout",
+  components: { AppMessage }
 }
 </script>
 
