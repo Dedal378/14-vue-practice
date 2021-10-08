f5c3d825693d7466cb7e86fd295eb767ee4e3e6e
Dedal378 <orlovetsfoto@gmail.com>
2021-10-08 20:11:13 +0300

    delete & update task, watch badge in AppStatus.vue

--- src/store/modules/request.module.js
+++ src/store/modules/request.module.js
@@ -21,10 +21,10 @@ export default {
       try {
         const token = store.getters["auth/token"]
         const { data } = await axios.post(`/requests.json?auth=${ token }`, payload)
-        commit('addRequest', {...payload, id: data.name})
+        commit('addRequest', { ...payload, id: data.name })
         dispatch('setMessage', {
           value: 'Заявка успешно создана',
-           type: 'primary'
+          type: 'primary'
         }, { root: true })
       } catch (e) {
         dispatch('setMessage', {
@@ -49,8 +49,38 @@ export default {
     async loadOne({ commit, dispatch }, id) {
       try {
         const token = store.getters['auth/token']
-        const { data } = await axios.get(`/requests/${id}.json?auth=${ token }`)
+        const { data } = await axios.get(`/requests/${ id }.json?auth=${ token }`)
         return data
+      } catch (e) {
+        dispatch('setMessage', {
+          value: e.message,
+          type: 'danger'
+        }, { root: true })
+      }
+    },
+    async remove({ dispatch }, id) {
+      try {
+        const token = store.getters['auth/token']
+        await axios.delete(`/requests/${ id }.json?auth=${ token }`)
+        dispatch('setMessage', {
+          value: 'Заявка удалена',
+          type: 'primary'
+        }, { root: true })
+      } catch (e) {
+        dispatch('setMessage', {
+          value: e.message,
+          type: 'danger'
+        }, { root: true })
+      }
+    },
+    async update({ dispatch }, request) {
+      try {
+        const token = store.getters['auth/token']
+        await axios.put(`/requests/${ request.id }.json?auth=${ token }`, request)
+        dispatch('setMessage', {
+          value: 'Заявка обновлена',
+          type: 'primary'
+        }, { root: true })
       } catch (e) {
         dispatch('setMessage', {
           value: e.message,
--- src/views/Request.vue
+++ src/views/Request.vue
@@ -1,11 +1,26 @@
 <template>
   <app-loader v-if="loading"></app-loader>
 
-  <app-page back title="Заявка" v-else-if="request">
-    <p><strong>Статус: <app-status :type="request.status" /></strong></p>
+  <app-page v-else-if="request" back title="Заявка">
+    <p><strong>Статус:
+      <app-status :type="request.status" />
+    </strong></p>
     <p><strong>Имя владельца: {{ request.fio }}</strong></p>
     <p><strong>Телефон: {{ request.phone }}</strong></p>
     <p><strong>Сумма: {{ currency(request.amount) }}</strong></p>
+
+    <div class="form-control">
+      <label>Статус</label>
+      <select v-model="status">
+        <option value="active">Активен</option>
+        <option value="pending">Выполняется</option>
+        <option value="done">Завершен</option>
+        <option value="cancelled">Отменен</option>
+      </select>
+    </div>
+
+    <button @click="remove" class="btn danger">Удалить</button>
+    <button v-if="hasChanged" @click="update" class="btn">Обновить</button>
   </app-page>
 
   <h3 v-else class="text-center text-white">
@@ -14,30 +29,45 @@
 </template>
 
 <script>
-import { useRoute } from "vue-router";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { currency } from "@/utils/currency.js";
 import AppPage from "@/components/ui/AppPage.vue";
 import AppLoader from "@/components/ui/AppLoader.vue";
 import AppStatus from "@/components/ui/AppStatus.vue";
-import { currency } from "@/utils/currency.js";
 
 export default {
   name: "Request",
   components: { AppPage, AppLoader, AppStatus },
   setup() {
     const route = useRoute()
+    const router = useRouter()
     const store = useStore()
     const loading = ref(true)
     const request = ref({})
+    const status = ref()
 
-    onMounted(async () => {
+    const hasChanged = computed(() => request.value.status !== status.value)
+
+    const remove = async() => {
+      await store.dispatch('request/remove', route.params.id)
+      router.push('/')
+    }
+    const update = async() => {
+      const data = { ...request.value, status: status.value, id: route.params.id }
+      await store.dispatch('request/update', data)
+      request.value.status = status.value
+    }
+
+    onMounted(async() => {
       loading.value = true
       request.value = await store.dispatch('request/loadOne', route.params.id)
+      status.value = request.value?.status
       loading.value = false
     })
 
-    return { loading, request, currency }
+    return { loading, request, currency, remove, update, status, hasChanged }
   },
 }
 </script>
